ed9aca4f05df32646aee4ca61c58a4e59a6da4c2
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-04-14 13:52:49 +0900

    LGNC API Stub cleanup

--- lgncapi/src/lgnc_directaudio.c
+++ lgncapi/src/lgnc_directaudio.c
@@ -1,22 +1,9 @@
 #include "lgnc_directaudio.h"
-#include "lgnc_wrapper.h"
 
-int LGNC_DIRECTAUDIO_CheckBuffer()
-{
-    return (int)_LGNC_DIRECTAUDIO_CheckBuffer();
-}
+int LGNC_DIRECTAUDIO_CheckBuffer() { return 0; }
 
-int LGNC_DIRECTAUDIO_Close()
-{
-    return (int)_LGNC_DIRECTAUDIO_Close();
-}
+int LGNC_DIRECTAUDIO_Close() { return 0; }
 
-int LGNC_DIRECTAUDIO_Open(LGNC_ADEC_DATA_INFO_T *info)
-{
-    return (int)_LGNC_DIRECTAUDIO_Open(info);
-}
+int LGNC_DIRECTAUDIO_Open(LGNC_ADEC_DATA_INFO_T *info) { return 0; }
 
-int LGNC_DIRECTAUDIO_Play(void *data, unsigned int size)
-{
-    return (int)_LGNC_DIRECTAUDIO_Play(data, size);
-}
+int LGNC_DIRECTAUDIO_Play(void *data, unsigned int size) { return 0; }
--- lgncapi/src/lgnc_directvideo.c
+++ lgncapi/src/lgnc_directvideo.c
@@ -1,17 +1,7 @@
 #include "lgnc_directvideo.h"
-#include "lgnc_wrapper.h"
 
-int LGNC_DIRECTVIDEO_Close()
-{
-    return (int)_LGNC_DIRECTVIDEO_Close();
-}
+int LGNC_DIRECTVIDEO_Close() { return 0; }
 
-int LGNC_DIRECTVIDEO_Open(LGNC_VDEC_DATA_INFO_T *info)
-{
-    return (int)_LGNC_DIRECTVIDEO_Open(info);
-}
+int LGNC_DIRECTVIDEO_Open(LGNC_VDEC_DATA_INFO_T *info) { return 0; }
 
-int LGNC_DIRECTVIDEO_Play(void *data, unsigned int size)
-{
-    return (int)_LGNC_DIRECTVIDEO_Play(data, size);
-}
+int LGNC_DIRECTVIDEO_Play(void *data, unsigned int size) { return 0; }
