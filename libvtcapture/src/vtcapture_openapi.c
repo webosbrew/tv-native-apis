01e8c517500180e25712bea1f1afa53e99474851
TBSniller <51515147+TBSniller@users.noreply.github.com>
2021-05-21 03:49:29 +0200

    Create vtcapture_openapi.c

---
+++ libvtcapture/src/vtcapture_openapi.c
@@ -0,0 +1,7 @@
+#include "vtcapture/vtcapture_openapi.h"
+
+VT_STATUS_T vtCapture_init(VTDRIVER *this) { return -1; }
+VT_STATUS_T vtCapture_preprocess(VTDRIVER *this, VT_IDK *idk, VT_CAPTURE_PROPERTIES *props) { return -1; }
+VT_STATUS_T vtCapture_process(VTDRIVER *this, VT_IDK *idk) { return -1; }
+VT_STATUS_T vtCapture_postprocess(VTDRIVER *this, VT_IDK *idk) { return -1; }
+VT_STATUS_T vtCapture_finalize(VTDRIVER *this, VT_IDK *idk) { return -1; }
