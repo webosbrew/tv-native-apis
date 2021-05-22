5c60b6f6857224c3a56bc8ca9b46321a7decf019
Piotr Dobrowolski <admin@tastycode.pl>
2021-05-22 13:40:07 +0200

    GM_ UI layers capture API

---
+++ libgm/src/gm.c
@@ -0,0 +1,5 @@
+#include "gm.h"
+
+int GM_CreateSurface(uint32_t width, uint32_t height, uint32_t dummy, GM_SURFACE* surface_info) { return -1; }
+int GM_DestroySurface(uint32_t surfaceID) { return -1; }
+int GM_CaptureGraphicScreen(uint32_t surfaceID, uint32_t* width, uint32_t* height) { return -1; }
