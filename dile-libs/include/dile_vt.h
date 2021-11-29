473eebdaf895e07754d5bc19b47e972b4194db6d
Piotr Dobrowolski <admin@tastycode.pl>
2021-11-29 18:18:23 +0100

    dile_vt: add SetVfodRegion and WaitVsync calls

--- dile-libs/include/dile_vt.h
+++ dile-libs/include/dile_vt.h
@@ -20,6 +20,13 @@ typedef struct DILE_VT_FRAMEBUFFER_PROPERTY_T
     void *ptr;
 } DILE_VT_FRAMEBUFFER_PROPERTY;
 
+typedef struct DILE_VT_REGION_T {
+    uint16_t x;
+    uint16_t y;
+    uint16_t width;
+    uint16_t height;
+} DILE_VT_REGION;
+
 DILE_VT_HANDLE DILE_VT_Create(unsigned int videoWindowId);
 DILE_VT_HANDLE DILE_VT_CreateEx(unsigned int videoWindowId, int bufferNum);
 int DILE_VT_GetVideoFrameOutputDeviceDumpLocation(DILE_VT_HANDLE, int *pDumpLocation);
@@ -27,3 +34,5 @@ int DILE_VT_SetVideoFrameOutputDeviceDumpLocation(DILE_VT_HANDLE, int pDumpLocat
 int DILE_VT_SetVideoFrameOutputDeviceState(DILE_VT_HANDLE, uint32_t stateFlags, DILE_OUTPUTDEVICE_STATE *state);
 int DILE_VT_GetCurrentVideoFrameBufferProperty(DILE_VT_HANDLE, DILE_VT_FRAMEBUFFER_PROPERTY *param_2, uint32_t *pIndexOfCurrentVideoFrameBuffer);
 void DILE_VT_Destroy(DILE_VT_HANDLE);
+int DILE_VT_SetVideoFrameOutputDeviceOutputRegion(DILE_VT_HANDLE, int pDumpLocation, DILE_VT_REGION* region);
+int DILE_VT_WaitVsync(DILE_VT_HANDLE, int unk1, int unk2);
--- dile-libs/src/dile_vt.c
+++ dile-libs/src/dile_vt.c
@@ -7,3 +7,5 @@ int DILE_VT_SetVideoFrameOutputDeviceDumpLocation(DILE_VT_HANDLE handle, int pDu
 int DILE_VT_SetVideoFrameOutputDeviceState(DILE_VT_HANDLE handle, uint32_t stateFlags, DILE_OUTPUTDEVICE_STATE *state) { return -1; }
 int DILE_VT_GetCurrentVideoFrameBufferProperty(DILE_VT_HANDLE handle, DILE_VT_FRAMEBUFFER_PROPERTY *param_2, uint32_t *pIndexOfCurrentVideoFrameBuffer) { return -1; }
 void DILE_VT_Destroy(DILE_VT_HANDLE handle) {}
+int DILE_VT_SetVideoFrameOutputDeviceOutputRegion(DILE_VT_HANDLE handle, int pDumpLocation, DILE_VT_REGION* region) { return -1; }
+int DILE_VT_WaitVsync(DILE_VT_HANDLE handle, int unk1, int unk2) { return -1; }
