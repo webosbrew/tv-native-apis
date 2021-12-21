0bbdb79463154fa421640444e88ce5056c1a264b
Mariotaku Lee <mariotaku.lee@gmail.com>
2021-12-21 17:48:13 +0900

    updating docs

--- lgncapi/include/_cgl_group.h
+++
@@ -1,7 +0,0 @@
-/**
- * @defgroup cgl libcgl
- * @brief Game and multimedia API
- *
- * CGL is native game and multimedia API for LG NetCast platform.
- * It also works for webOS 1.x to webOS 4.x.
- */
