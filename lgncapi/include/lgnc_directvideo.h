3bc269a594f40cd03e3361df4383ba762b7bd311
Ningyuan Li <mariotaku.lee@gmail.com>
2023-02-11 23:24:54 +0900

    added _LGNC_DIRECTVIDEO_SetDisplayWindow

--- lgncapi/include/lgnc_directvideo.h
+++ lgncapi/include/lgnc_directvideo.h
@@ -32,5 +32,26 @@ struct LGNC_VDEC_DATA_INFO
 typedef struct LGNC_VDEC_DATA_INFO LGNC_VDEC_DATA_INFO_T;
 
 int LGNC_DIRECTVIDEO_Close();
+
 int LGNC_DIRECTVIDEO_Open(const LGNC_VDEC_DATA_INFO_T *info);
+
 int LGNC_DIRECTVIDEO_Play(const void *data, unsigned int size);
+
+/**
+ * @param x X position of the video
+ * @param y Y position of the video
+ * @param w Width of the video
+ * @param h Height of the video
+ * @return 0 If succeeded
+ */
+int _LGNC_DIRECTVIDEO_SetDisplayWindow(int x, int y, int w, int h);
+
+/**
+ * @param x X position of the video
+ * @param y Y position of the video
+ * @param w Width of the video
+ * @param h Height of the video
+ * @return 0 If succeeded
+ * @since webOS 1.4.0
+ */
+int _LGNC_DIRECTVIDEO_SetCustomDisplayWindow(int x, int y, int w, int h);
--- lgncapi/src/lgnc_directvideo.c
+++ lgncapi/src/lgnc_directvideo.c
@@ -5,3 +5,7 @@ int LGNC_DIRECTVIDEO_Close() { return 0; }
 int LGNC_DIRECTVIDEO_Open(const LGNC_VDEC_DATA_INFO_T *info) { return 0; }
 
 int LGNC_DIRECTVIDEO_Play(const void *data, unsigned int size) { return 0; }
+
+int _LGNC_DIRECTVIDEO_SetDisplayWindow(int x, int y, int w, int h) { return 0; }
+
+int _LGNC_DIRECTVIDEO_SetCustomDisplayWindow(int x, int y, int w, int h) { return 0; }
