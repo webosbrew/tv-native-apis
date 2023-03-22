0af60c5b9af7b9eb01c1c6cdb6f2a13734c752e3
Ningyuan Li <mariotaku.lee@gmail.com>
2023-03-23 00:54:12 +0900

    fixed enum values

--- appswitching-control-block/include/AcbAPI.h
+++ appswitching-control-block/include/AcbAPI.h
@@ -2,13 +2,13 @@
 
 #include <stdbool.h>
 
-typedef void (*AcbAPI_Callback)(long acbId, long taskId, long eventType, long appState, long playState, const char *reply);
+typedef void (*AcbAPI_Callback)(long acbId, long taskId, long eventType, long appState, long playState,
+                                const char *reply);
 
-typedef enum AcbAPI_PlayerType
-{
-    PLAYER_TYPE_GROUP = 0L,
-    PLAYER_TYPE_VIDEO,
-    PLAYER_TYPE_VIDEO_UNMUTED_AFTER_PLAYING,
+typedef enum AcbAPI_PlayerType {
+    PLAYER_TYPE_GROUP = 0,
+    PLAYER_TYPE_VIDEO = 0,
+    PLAYER_TYPE_VIDEO_UNMUTED_AFTER_PLAYING = 1,
     PLAYER_TYPE_AUDIO,
     PLAYER_TYPE_HTML5_AUDIO,
     PLAYER_TYPE_TV,
@@ -22,32 +22,25 @@ typedef enum AcbAPI_PlayerType
     PLAYER_TYPE_PHOTO,
 } AcbAPI_PlayerType;
 
-typedef enum AcbAPI_SinkType
-{
-    SINK_TYPE_AUTO = 0L,
-    SINK_TYPE_MAIN = 1L,
-    SINK_TYPE_SUB = 2L,
+typedef enum AcbAPI_SinkType {
+    SINK_TYPE_MAIN = 0,
+    SINK_TYPE_SUB = 1,
 } AcbAPI_SinkType;
 
-typedef enum AcbAPI_AppState
-{
+typedef enum AcbAPI_AppState {
     APPSTATE_INITIALIZED = 0,
     APPSTATE_FOREGROUND = 1,
     APPSTATE_BACKGROUND = 2,
 } AcbAPI_AppState;
 
-typedef enum AcbAPI_PlayState
-{
+typedef enum AcbAPI_PlayState {
     PLAYSTATE_UNLOADED = 0,
     PLAYSTATE_LOADED = 1,
-    PLAYSTATE_SEAMLESS_LOADED = 2,
-    PLAYSTATE_PLAYING = 3,
-    PLAYSTATE_PAUSED = 4,
-    PLAYSTATE_AUDIO_ONLY_PLAYING = 5,
-    PLAYSTATE_FROZEN = 6,
+    PLAYSTATE_PLAYING = 2,
+    PLAYSTATE_PAUSED = 3,
 } AcbAPI_PlayState;
 
-long AcbAPI_create(void);
+long AcbAPI_create();
 
 bool AcbAPI_initialize(long acbId, AcbAPI_PlayerType playerType, const char *appId, AcbAPI_Callback callback);
 
@@ -61,7 +54,8 @@ bool AcbAPI_setSinkType(long acbId, AcbAPI_SinkType sinkType);
 
 int AcbAPI_setDisplayWindow(long acbId, long x, long y, long w, long h, bool fullScreen, long *taskId);
 
-int AcbAPI_setCustomDisplayWindow(long acbId, long in_x, long in_y, long in_w, long in_h, long out_x, long out_y, long out_w, long out_h, bool fullScreen, long *taskId);
+int AcbAPI_setCustomDisplayWindow(long acbId, long in_x, long in_y, long in_w, long in_h, long out_x, long out_y,
+                                  long out_w, long out_h, bool fullScreen, long *taskId);
 
 int AcbAPI_finalize(long acbId);
 
